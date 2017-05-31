4c3d34e9823e82b982b21047fe5a04307cc6e03d
David Benjamin Roberts <david.b.roberts@outlook.com>
2017-05-30 22:18:27 -0600

    slowing dow

--- main.py
+++ main.py
@@ -20,12 +20,16 @@ import redfield
 import primitive
 import linblad_solver
 
+
 # Load global variables
+TIME_STEP = parameters.TIME_STEP
 NUM_STATES_CUTOFF = parameters.NUM_STATES_CUTOFF
 ANNEALING_PARAMETER = parameters.ANNEALING_PARAMETER
 
 
-def apply(linbladian, density_matrix, s):
+
+
+def tensor_apply(linbladian, density_matrix, s):
 
 	def output_components_before_contraction(i,j,k,l):
 		return linbladian[s][i][j][k][l]*density_matrix[i][j]
@@ -40,11 +44,15 @@ def apply(linbladian, density_matrix, s):
 	return [[output_components(k,l) for l in range(NUM_STATES_CUTOFF)] for k in range(NUM_STATES_CUTOFF)]
 
 
-def compute_linblad_evolution(initial_density_matrix):
+def compute_linblad_evolution(initial_density_matrix, linbladian):
 	density_matrix_linblad_evolution = (([0]*NUM_STATES_CUTOFF)*NUM_STATES_CUTOFF)*len(ANNEALING_PARAMETER)
 	transient_density_matrix = initial_density_matrix
+	density_matrix_linblad_evolution[0] = initial_density_matrix
 	for s in ANNEALING_PARAMETER:
-
+		infinitessimal_change = tensor_apply(linbladian, transient_density_matrix, s)
+		transient_density_matrix = transient_density_matrix + TIME_STEP*infinitessimal_change
+		density_matrix_linblad_evolution[s] = transient_density_matrix
+	return density_matrix_linblad_evolution
 
 
 
--- redfield.py
+++ redfield.py
@@ -21,6 +21,8 @@ import helper
 # Load global variables
 NUM_QUBITS = parameters.NUM_QUBITS
 NUM_STATES = parameters.NUM_STATES
+SPECTRAL_DENSITY = parameters.SPECTRAL_DENSITY
+
 
 def sigmaz_matrix_element(s,qubit_index,i,j, sorted_eigenvectors):
             helper.Z(qubit_index).matrix_element(sorted_eigenvectors[s][i],sorted_eigenvectors[s][j])
@@ -29,9 +31,9 @@ def sigmaz_matrix_element(s,qubit_index,i,j, sorted_eigenvectors):
 def redfield_coefficient_tensor(sorted_eigenvectors):
  
     def redfield_coefficient_tensor_components(s,i,j,k,l):
-        return sum([sigmaz_matrix_element(s,qubit,i,k, sorted_eigenvectors)*sigmaz_matrix_element(s,qubit,j,l, sorted_eigenvectors) for qubit in qubits])
+        return sum([sigmaz_matrix_element(s,qubit,i,k, sorted_eigenvectors)*sigmaz_matrix_element(s,qubit,j,l, sorted_eigenvectors) for qubit in range(NUM_QUBITS)])
  
-    return [[[[[redfield_coefficient_tensor_components(s,i,j,k,l) for l in states] for k in states] for j in states] for i in states] for s in ANNEALING_PARAMETER]
+    return [[[[[redfield_coefficient_tensor_components(s,i,j,k,l) for l in range(NUM_STATES)] for k in range(NUM_STATES)] for j in range(NUM_STATES)] for i in range(NUM_STATES)] for s in ANNEALING_PARAMETER]
 
 
 def redfield_tensor(sorted_eigenvectors, frequency_matrix):
@@ -41,14 +43,14 @@ def redfield_tensor(sorted_eigenvectors, frequency_matrix):
     coefficient_tensor = redfield_coefficient_tensor(sorted_eigenvectors)
     
     def gamma_plus(s,i,j,k,l):
-        return S(-frequency_matrix[s][k][l])*coefficient_tensor[s][i][j][k][l]
+        return SPECTRAL_DENSITY(-frequency_matrix[s][k][l])*coefficient_tensor[s][i][j][k][l]
     def gamma_minus(s,i,j,k,l):
-        return S(frequency_matrix[s][i][j])*coefficient_tensor[s][i][j][k][l]
+        return SPECTRAL_DENSITY(frequency_matrix[s][i][j])*coefficient_tensor[s][i][j][k][l]
 
     def redfield_tensor_term_no1_components(s,i,j,k,l):
         return sum([helper.delta(j,l)*gamma_plus(s,i,k,n,n) for n in range(2**num_qubits)])
 
-    def redfield_tensor_term_no1_components(s,i,j,k,l):
+    def redfield_tensor_term_no2_components(s,i,j,k,l):
         return sum([helper.delta(i,k)*gamma_minus(s,j,l,n,n) for n in range(2**num_qubits)])
 
     def redfield_tensor_term_no3_components(s,i,j,k,l):
