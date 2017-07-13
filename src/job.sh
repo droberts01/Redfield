1f0a9343366c22a93fe77cfc10045d54890c796c
David Benjamin Roberts <david.b.roberts@outlook.com>
2017-07-13 04:23:53 -0600

    Modified shell script

--- src/job.sh
+++ src/job.sh
@@ -17,10 +17,15 @@ echo "Starting Redfield simulation of the D Wave 2X:"
 STARTTIME=$(date +%s)
 # generate.py -tQA, -I, -J, -K, -N, -Nc, 
 #   			-step, -window_size, -num_samples, -decoherence
+# Generate a Bloch-Redfield master equation:
 python generate.py 5E-6 .2 .23 1 5 4 10 0.001 20 10000 0
 
 # for D in 0
 # do
+
+# solve.py -tQA, -I, -J, -K, -N, -Nc, 
+#   			-step, -window_size, -num_samples, -decoherence
+# Solve the Bloch-Redfield master equation (via 2nd order Implicit Runge-Kutta):
 # python solve.py 5 .2 .23 4 $D
 # done
 
