47d82a013e1a2a9658e9bd00f2ade172dfbab37a
David Benjamin Roberts <david.b.roberts@outlook.com>
2017-07-15 01:56:11 -0600

    Make job.sh executable

--- src/job.sh
+++ src/job.sh
@@ -10,26 +10,38 @@ env | sort > ENVIRONMENT
 
 echo "Starting Redfield simulation of the D Wave 2X:" 
 
-# Needed on certain supercomputing clusers:
 
+# Needed on certain supercomputing clusers:
 # module load anaconda
 
+
+#----------------------------------START-----------------------~~~~~
 STARTTIME=$(date +%s)
+
+#-----------------------------------------------------------------
 # generate.py -tQA, -I, -J, -K, -N, -Nc, 
 #   			-step, -window_size, -num_samples, -decoherence
 # Generate a Bloch-Redfield master equation:
-python generate.py 5E-6 .2 .23 1 5 4 10 0.001 20 10000 0
 
-# for D in 0
-# do
+python generate.py 5E-6 .2 .23 1 5 4 10 0.001 20 15000 0
 
+#-----------------------------------------------------------------
 # solve.py -tQA, -I, -J, -K, -N, -Nc, 
 #   			-step, -window_size, -num_samples, -decoherence
 # Solve the Bloch-Redfield master equation (via 2nd order Implicit Runge-Kutta):
-# python solve.py 5 .2 .23 4 $D
+python solve.py 5E-6 .2 .23 1 5 4 10 0.001 20 15000 0
+
+
+#----------------------------------FOR LOOP-----------------------
+# for D in 0
+# do
+
 # done
 
+
 ENDTIME=$(date +%s)
+#----------------------------------END-----------------------~~~~~
+
 
 
 echo "Stopping:"
