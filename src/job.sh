0a0fc57d378464ee14ea27da080db576bb0ec42c
David Benjamin Roberts <david.b.roberts@outlook.com>
2017-08-06 13:01:42 -0600

    Final version for running on the cluster.

--- src/job.sh
+++ src/job.sh
@@ -19,23 +19,21 @@ echo "Starting Redfield simulation of the D Wave 2X:"
 STARTTIME=$(date +%s)
 
 
-# generate.py -tQA, -I, -J, -K, -N, -Nc, 
-#   			-step, -window_size, -num_samples, -decoherence -LF_noise
-# Generate a Bloch-Redfield master equation:
+
 for N in 5 7 9
 do
 	for J in .24 .28 .32 .36 .4 .44 .48
 	do 
-		for LF in 0 1
-		do
-			python generate.py 5E-6 .2 $J 1 $N 4 0.001 10 10000 1 $LF "Home"
+		# generate.py -tQA, -I, -J, -K, -N, -Nc, 
+		#   			-step, -window_size, -num_samples, -decoherence -LF_noise
+		# Generate a Bloch-Redfield master equation:
+		python generate.py 5E-6 .2 $J 1 $N 6 0.001 10 10000 1 1 "Home"
 
 
-	# solve.py -tQA, -I, -J, -K, -N, -Nc, 
-	#   			-step, -window_size, -num_samples, -decoherence -LF_noise
-	# Solve the Bloch-Redfield master equation (via 2nd order Implicit Runge-Kutta):
-			python solve.py 5E-6 .2 $J 1 $N 4 0.001 10 10000 1 $LF "Home"
-		done
+		# solve.py -tQA, -I, -J, -K, -N, -Nc, 
+		#   			-step, -window_size, -num_samples, -decoherence -LF_noise
+		# Solve the Bloch-Redfield master equation (via 2nd order Implicit Runge-Kutta):
+		python solve.py 5E-6 .2 $J 1 $N 6 0.001 10 10000 1 1 "Home"
 	done
 done
 
@@ -43,7 +41,14 @@ done
 # do
 
 # done
-
+# for N in 9
+# do
+# 	for Nc in 4 6
+# 	do
+# 		python generate.py 5E-6 .2 .24 1 $N $Nc 0.0002 10 10000 1 1 "Home"
+# 		python solve.py 5E-6 .2 .24 1 $N $Nc 0.0002 10 10000 1 1 "Home"
+# 	done
+# done
 
 ENDTIME=$(date +%s)
 
